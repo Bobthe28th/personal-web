6bb85845962e466c56565bc2883cd3c447b6f52d
Bobthe28th <owenmjones28@gmail.com>
2025-01-18 16:55:29 -0800

    Change hello world

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+    res.send('Hello World! I think its goober time');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);

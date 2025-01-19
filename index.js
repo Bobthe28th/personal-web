e58a30f8bbab6e6d922cd84d6574cb7aa445fa11
Bobthe28th <owenmjones28@gmail.com>
2025-01-18 19:25:51 -0800

    custom args for https and port

--- index.js
+++ index.js
@@ -5,21 +5,34 @@ const fs = require('fs');
 const path = require('path');
 const exphbs = require("express-handlebars")
 const app = express();
-const port = parseInt(process.argv[2] ?? 3000);
+const args = process.argv.slice(2);
+const useHttps = args.includes("--https");
 
+const getPort = () => {
+    const portArg = args.find(arg => arg.startsWith('--port='));
+    if (portArg) {
+      return portArg.split('=')[1]; // Extract the value after the '=' sign
+    }
+    return 80; // Return null if no port argument is provided
+};
+
+const port = getPort();
 http.createServer((req, res) => {
     res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
     res.end();
-}).listen(80);
-
-const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/bobthe28th.me/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/bobthe28th.me/fullchain.pem'),
-};
-
-https.createServer(options, app).listen(443, () => {
-    console.log('HTTPS Server running on port 443');
+}).listen(port, () => {
+    console.log(`HTTP Server running on port ${port}`);
 });
+
+if (useHttps) {
+    const options = {
+        key: fs.readFileSync('/etc/letsencrypt/live/bobthe28th.me/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/bobthe28th.me/fullchain.pem'),
+    };
+    https.createServer(options, app).listen(443, () => {
+        console.log('HTTPS Server running on port 443');
+    });
+}
 
 app.use(express.json());
 
@@ -34,10 +47,6 @@ app.set("view engine", "handlebars");
 app.get('/', (req, res) => {
     res.render('home', {});
 });
-
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`);
-// });
 
 app.get("*", function (req, res) {
     res.redirect("/");

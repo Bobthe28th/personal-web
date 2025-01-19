ce4a77842bb28ca7122ecf9fd2b520d8182af177
Bobthe28th <owenmjones28@gmail.com>
2025-01-18 19:10:57 -0800

    https

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const exphbs = require("express-handlebars")
 const app = express();
 const port = parseInt(process.argv[2] ?? 3000);
+
+http.createServer((req, res) => {
+    res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+    res.end();
+}).listen(80);
+
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/bobthe28th.me/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/bobthe28th.me/fullchain.pem'),
+};
+
+https.createServer(options, app).listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+});
 
 app.use(express.json());
 
@@ -17,9 +35,9 @@ app.get('/', (req, res) => {
     res.render('home', {});
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`);
+// });
 
 app.get("*", function (req, res) {
     res.redirect("/");

e4a9548a36f86ae4db6e0678cc55500e3c6e56a5
Bobthe28th <owenmjones28@gmail.com>
2025-01-18 17:46:20 -0800

    Port argument

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = process.argv[2] ?? 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World! I think its goober time!!!');

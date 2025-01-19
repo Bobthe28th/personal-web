1eec7d82664f799de5b3f4367b0d1a100bc34b8a
Bobthe28th <owenmjones28@gmail.com>
2025-01-18 18:24:50 -0800

    Handlebars and basic home page

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const exphbs = require("express-handlebars")
 const app = express();
 const port = parseInt(process.argv[2] ?? 3000);
 
+app.use(express.json());
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 app.get('/', (req, res) => {
-    res.send('Hello World! I think its goober time!!!');
+    res.render('home', {});
 });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
+});
+
+app.get("*", function (req, res) {
+    res.redirect("/");
 });
